91d3b0feb03ea5b7381707d1ee211d9267fa6515
aalhommada <aalhommada@gmail.com>
2023-02-12 13:26:25 +0100

    update content

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,3 +1,45 @@
+import styles from "./contact.module.css";
+import { FaLinkedin, FaTwitter, FaGithub, FaMedium } from "react-icons/fa";
+
 export function Contact() {
-  return <h2>contact info</h2>;
+  return (
+    <>
+      <h2 className={styles.contatHeader}>contact info</h2>
+      <div className={styles.icons}>
+        <a
+          href="https://twitter.com/a_alhommada"
+          rel="noreferrer"
+          target="_blank"
+          className="mr-6 text-md text-center text-white font-medLarg"
+        >
+          <FaTwitter />
+        </a>
+
+        <a
+          href="https://linkedin.com/in/aalhommada"
+          rel="noreferrer"
+          target="_blank"
+          className="mr-6 text-md text-center text-white font-medLarg"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/aalhommada"
+          rel="noreferrer"
+          target="_blank"
+          className="mr-6 text-md text-center text-white font-medLarg"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://medium.com/@aalhommada"
+          rel="noreferrer"
+          target="_blank"
+          className="text-md text-center text-white font-medLarg"
+        >
+          <FaMedium />
+        </a>
+      </div>
+    </>
+  );
 }
